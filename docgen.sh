7567107732b8a4b5dbc7b2fdcb68b1302ca6ceab
Brad Howes <bradhowes@mac.com>
2018-10-31 17:03:01 +0000

    Updated docs

--- docgen.sh
+++ docgen.sh
@@ -6,4 +6,4 @@ mkdir ./docs
 cp animation.gif docs/
 
 JAZZY=$(type -p jazzy)
-[[ -n "${JAZZY}" ]] && ${JAZZY} -x -workspace,JoyStickView.xcworkspace,-scheme,JoyStickView
+[[ -n "${JAZZY}" ]] && ${JAZZY} --module JoyStickView -x -workspace,JoyStickView.xcworkspace,-scheme,JoyStickView
