520ea26de3ed2ab3dedbad03879a79b883791bc9
Brad Howes <bradhowes@mac.com>
2020-07-28 21:33:31 +0200

    Added comment

--- docgen.sh
+++ docgen.sh
@@ -5,6 +5,10 @@ rm -rf ./docs
 mkdir ./docs
 cp animation.gif docs/
 
+# NOTE: the main branch for this repo is `main` and not `master`. However, Github Pages expects to
+# look at a `master` branch for a `/docs` directory. So, we have two branches (main and master) and
+# we must always keep them the same when the docs change.
+
 JAZZY=$(type -p jazzy)
 [[ -n "${JAZZY}" ]] && \
     ${JAZZY} --module JoyStickView \
