65a06489492ed964770d7a93b68a19d6a32e1c87
Brad Howes <bradhowes@mac.com>
2021-08-11 19:48:32 +0300

    Light cleanup

--- JoyStickView/Src/Bundle+Additions.swift
+++ JoyStickView/Src/Bundle+Additions.swift
@@ -8,11 +8,11 @@ extension Bundle {
      Locate an inner Bundle generated from CocoaPod packaging.
      
      - parameter name: the name of the inner resource bundle. This should match the "s.resource_bundle" key or
-     one of the "s.resoruce_bundles" keys from the podspec file that defines the CocoPod.
+     one of the "s.resource_bundles" keys from the podspec file that defines the CocoPod.
      - returns: the resource Bundle or `self` if resource bundle was not found
      */
     func podResource(name: String) -> Bundle {
         guard let bundleUrl = self.url(forResource: name, withExtension: "bundle") else { return self }
-        return Bundle(url: bundleUrl) ?? self
+        return .init(url: bundleUrl) ?? self
     }
 }
--- JoyStickView/Src/CoreGraphics+Additions.swift
+++ JoyStickView/Src/CoreGraphics+Additions.swift
@@ -5,9 +5,7 @@ import CoreGraphics
 /// Convenience functions for CGRect.
 public extension CGRect {
     /// Obtain the center of a CGRect.
-    var mid: CGPoint {
-        return CGPoint(x: midX, y: midY)
-    }
+    var mid: CGPoint { .init(x: midX, y: midY) }
 }
 
 /// Convenience functions for CGVector.
@@ -15,12 +13,10 @@ public extension CGVector {
     /**
      Multiply the components of a CGVector by a scalar
      - parameter lhs: the CGVector to multiply
-     - parameter rhs: the scalar to muliply
+     - parameter rhs: the scalar to multiply
      - returns new CGVector
      */
-    static func *(lhs: CGVector, rhs: CGFloat) -> CGVector {
-        return CGVector(dx: lhs.dx * rhs, dy: lhs.dy * rhs)
-    }
+    static func *(lhs: CGVector, rhs: CGFloat) -> CGVector { .init(dx: lhs.dx * rhs, dy: lhs.dy * rhs) }
 
     /**
      Multiply the components of a CGVector by a scalar
@@ -28,14 +24,12 @@ public extension CGVector {
      - parameter rhs: the scalar to muliply
      - returns new CGVector
      */
-    static func *(lhs: CGVector, rhs: Double) -> CGVector {
-        return lhs * CGFloat(rhs)
-    }
+    static func *(lhs: CGVector, rhs: Double) -> CGVector { lhs * CGFloat(rhs) }
 
     /// Obtain the squared magnitude of the CGVector
-    var magnitude2: CGFloat { return dx * dx + dy * dy }
+    var magnitude2: CGFloat { dx * dx + dy * dy }
     /// Obtain the magnitude of the CGVector
-    var magnitude: CGFloat { return sqrt(magnitude2) }
+    var magnitude: CGFloat { sqrt(magnitude2) }
 }
 
 /// Convenience functions for CGPoint.
@@ -46,9 +40,7 @@ public extension CGPoint {
      - parameter rhs: the CGVector to add
      - returns: new CGPoint representing the sum
      */
-    static func + (lhs: CGPoint, rhs: CGVector) -> CGPoint {
-        return CGPoint(x: lhs.x + rhs.dx, y: lhs.y + rhs.dy)
-    }
+    static func + (lhs: CGPoint, rhs: CGVector) -> CGPoint { .init(x: lhs.x + rhs.dx, y: lhs.y + rhs.dy) }
 
     /**
      Add the components of a CGPoint and a CGSize
@@ -56,9 +48,7 @@ public extension CGPoint {
      - parameter rhs: the CGSize to add
      - returns: new CGPoint representing the sum
      */
-    static func + (lhs: CGPoint, rhs: CGSize) -> CGPoint {
-        return CGPoint(x: lhs.x + rhs.width, y: lhs.y + rhs.height)
-    }
+    static func + (lhs: CGPoint, rhs: CGSize) -> CGPoint { .init(x: lhs.x + rhs.width, y: lhs.y + rhs.height) }
 
     /**
      Subtract the components of a CGPoint and a CGVector
@@ -66,9 +56,7 @@ public extension CGPoint {
      - parameter rhs: the CGVector to subtract
      - returns: new CGPoint representing the difference
      */
-    static func - (lhs: CGPoint, rhs: CGVector) -> CGPoint {
-        return CGPoint(x: lhs.x - rhs.dx, y: lhs.y - rhs.dy)
-    }
+    static func - (lhs: CGPoint, rhs: CGVector) -> CGPoint { .init(x: lhs.x - rhs.dx, y: lhs.y - rhs.dy) }
 
     /**
      Subtract the components of a CGPoint and a CGSize
@@ -76,9 +64,7 @@ public extension CGPoint {
      - parameter rhs: the CGSize to subtract
      - returns: new CGPoint representing the difference
      */
-    static func - (lhs: CGPoint, rhs: CGSize) -> CGPoint {
-        return CGPoint(x: lhs.x - rhs.width, y: lhs.y - rhs.height)
-    }
+    static func - (lhs: CGPoint, rhs: CGSize) -> CGPoint { .init(x: lhs.x - rhs.width, y: lhs.y - rhs.height) }
 
     /**
      Subtract the components of two CGPoint values
@@ -86,9 +72,7 @@ public extension CGPoint {
      - parameter rhs: the CGPoint to subtract
      - returns: new CGVector representing the difference
      */
-    static func - (lhs: CGPoint, rhs: CGPoint) -> CGVector {
-        return CGVector(dx: lhs.x - rhs.x, dy: lhs.y - rhs.y)
-    }
+    static func - (lhs: CGPoint, rhs: CGPoint) -> CGVector { .init(dx: lhs.x - rhs.x, dy: lhs.y - rhs.y) }
 }
 
 /// Convenience functions for CGSize.
@@ -100,7 +84,7 @@ public extension CGSize {
      - returns: new CGSize representing the sum
      */
     static func + (lhs: CGSize, rhs: CGSize) -> CGSize {
-        return CGSize(width: lhs.width + rhs.width, height: lhs.height + rhs.height)
+        .init(width: lhs.width + rhs.width, height: lhs.height + rhs.height)
     }
     /**
      Multiply the components of a CGSize value by a scalar
@@ -109,7 +93,7 @@ public extension CGSize {
      - returns: new CGSize representing the result
      */
     static func * (lhs: CGSize, rhs: CGFloat) -> CGSize {
-        return CGSize(width: lhs.width * rhs, height: lhs.height * rhs)
+        .init(width: lhs.width * rhs, height: lhs.height * rhs)
     }
     /**
      Divide the components of a CGSize value by a scalar
@@ -118,7 +102,7 @@ public extension CGSize {
      - returns: new CGSize representing the result
      */
     static func / (lhs: CGSize, rhs: CGFloat) -> CGSize {
-        return CGSize(width: lhs.width / rhs, height: lhs.height / rhs)
+        .init(width: lhs.width / rhs, height: lhs.height / rhs)
     }
     /**
      Divide the components of two CGSize values
@@ -127,6 +111,6 @@ public extension CGSize {
      - returns: new CGSize representing the result
      */
     static func / (lhs: CGSize, rhs: CGSize) -> CGSize {
-        return CGSize(width: lhs.width / rhs.width, height: lhs.height / rhs.height)
+        .init(width: lhs.width / rhs.width, height: lhs.height / rhs.height)
     }
 }
