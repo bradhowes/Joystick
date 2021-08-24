ce3aa15408a8a376fc64634600c7c735d46ffa36
Brad Howes <bradhowes@mac.com>
2021-08-24 19:16:48 -0400

    Updated doc

--- docgen.sh
+++ docgen.sh
@@ -12,5 +12,5 @@ cp animation.gif docs/
 JAZZY=$(type -p jazzy)
 [[ -n "${JAZZY}" ]] && \
     ${JAZZY} --module JoyStickView \
-             --build-tool-arguments -workspace,JoyStickView.xcworkspace,-scheme,JoyStickView \
+             --build-tool-arguments -workspace,JoyStickView.xcworkspace,-scheme,"JoyStickView App" \
              --root-url https://bradhowes.github.io/Joystick
