77b547bd4ea01330847439ba461a804fe0080dc8
Brad Howes <bradhowes@mac.com>
2021-08-25 16:03:51 -0400

    Ignore .swiftpm directory

--- Package.swift
+++ Package.swift
@@ -3,22 +3,25 @@
 import PackageDescription
 
 let package = Package(
-    name: "JoyStickView",
-    platforms: [.iOS(.v12)],
-    products: [
-        .library(
-            name: "JoyStickView",
-            targets: ["JoyStickView"]),
-    ],
-    dependencies: [
-    ],
-    targets: [
-        .target(
-            name: "JoyStickView",
-            dependencies: []
-        ),
-        .testTarget(
-            name: "JoyStickViewTests",
-            dependencies: ["JoyStickView"]),
-    ]
+  name: "JoyStickView",
+  platforms: [.iOS(.v12)],
+  products: [
+    .library(
+      name: "JoyStickView",
+      targets: ["JoyStickView"]),
+  ],
+  dependencies: [
+  ],
+  targets: [
+    .target(
+      name: "JoyStickView",
+      dependencies: [],
+      exclude: ["Resources/Info.plist"]
+    ),
+    .testTarget(
+      name: "JoyStickViewTests",
+      dependencies: ["JoyStickView"],
+      exclude: ["Info.plist"]
+    )
+  ]
 )
